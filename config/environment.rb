28bc73ffc52cf6af4fbe431bf52e48c4c0f20422
joshseyda <josh.seyda@gmail.com>
2018-07-17 14:49:37 -0400

    troubleshooting heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# config/environments/production.rb
+INVESTMENT::Application.configure do
+    
+  # your config settings
+
+  config.assets.compile = true
+
+  # your other config settings
+end
