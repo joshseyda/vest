24a55632ebf26b58736d5a4c044ba6914ab6a84f
joshseyda <josh.seyda@gmail.com>
2018-07-17 15:45:39 -0400

    undoing previous troubleshooting changes

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# config/environments/production.rb
-INVESTMENT::Application.configure do
-    
-  # your config settings
-
-  config.assets.compile = true
-
-  # your other config settings
-end
